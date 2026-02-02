35ff28f31acf221cb66e9b60ae97b3ab13224248
Mauricio Pereira <mauriciojuniordesigner@gmail.com>
2026-02-02 19:37:28 -0300

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { chunkSplitPlugin } from 'vite-plugin-chunk-split'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), chunkSplitPlugin()],
+  base: '/portfolio/',
   build: {
     chunkSizeWarningLimit: 1000,
     sourcemap: false,
